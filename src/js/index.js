257a9d24d2e10c6b76dceb624de770b14458f053
Adri-devnull <adri.devnull@gmail.com>
2023-11-28 00:18:24 +0100

    first-changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,27 @@
+// //ELEMENTOS DEL DOM
+const spanElement = document.getElementById('span');
+const h2Element = document.getElementById('h2');
+const buttonElement = document.getElementById('button');
+
+const request = url => fetch(url);
+
+// FUNCION FETCH PARA RECIBIR LOS DATOS
+const fetchData = async () => {
+    try {
+        const response = await request('https://api.adviceslip.com/advice');
+        if (response.status === 200) {
+            const data = await response.json();
+            spanElement.textContent = `Advice #${data.slip.id}`;
+            h2Element.textContent = `"${data.slip.advice}"`;
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+// EVENTO DE ESCUCHA PARA LLAMAR A LA FUNCION FETCHDATA 
+buttonElement.addEventListener('click', () => {
+    fetchData();
+});
+
 
